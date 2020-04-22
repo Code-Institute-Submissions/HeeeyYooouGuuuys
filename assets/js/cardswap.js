4c742754a409b03952f8316fd2aa01ba0cd855d5
pengols <pengelly30@hotmail.com>
2020-04-22 12:11:00 +0000

    basic modal added to about nav button

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -1,1 +1,25 @@
-console.log("hello");
+// Get the modal
+let modal = document.getElementById("aboutModal");
+
+// target "About" Nav-item
+let btn = document.getElementById("aboutButton");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
