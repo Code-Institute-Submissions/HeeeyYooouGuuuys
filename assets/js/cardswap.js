afec935392510d6b6df0b2cf2a17655113c09367
pengols <pengelly30@hotmail.com>
2020-04-28 13:21:51 +0000

    added credits modal template and JS to play.html for testing

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -1,3 +1,7 @@
+// Modals
+
+// About modal
+
 // Get the modal
 let modal = document.getElementById("aboutModal");
 
@@ -20,6 +24,34 @@ span.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+// Credit Modal
+
+// Get the modal
+let creditModal = document.getElementById("creditModal");
+
+// target "About" Nav-item
+let creditBtn = document.getElementById("creditButton");
+
+// Get the <span> element that closes the modal
+// let span = document.getElementsByClassName("close")[0];
+
+// open the modal
+creditBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == creditModal) {
     modal.style.display = "none";
   }
 } 
