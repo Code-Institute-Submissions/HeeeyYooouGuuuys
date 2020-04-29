21952a128093ee788151ebca6ab4faedaa189ada
pengols <pengelly30@hotmail.com>
2020-04-29 15:35:21 +0000

    combined JS for both about and credit modals on play.html

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -1,62 +1,37 @@
 // Modals
 
-// About modal
-
 // Get the modal
-let modal = document.getElementById("aboutModal");
+var modal = document.getElementsByClassName('modal');
 
-// target "About" Nav-item
-let btn = document.getElementById("aboutButton");
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("modalOpen");
+
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
-// open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal 
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
 }
 
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span[0].onclick = function() {
+    modal[0].style.display = "none";
 }
 
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
-
-// Credit Modal
-
-// Get the modal
-let creditModal = document.getElementById("creditModal");
-
-// target "credit" footer item
-let creditBtn = document.getElementById("creditButton");
-
-// Get the <span> element that closes the modal
-let creditSpan = document.getElementsByClassName("close")[0];
-
-// open the modal
-creditBtn.onclick = function() {
-  creditModal.style.display = "block";
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  creditModal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == creditModal) {
-    creditModal.style.display = "none";
-  }
-} 
-
-
 
 // Audio //
 
