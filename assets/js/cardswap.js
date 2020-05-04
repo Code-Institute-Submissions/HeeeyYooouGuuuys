5da5ebf0e36e24af1f72abe93e635f63e8935502
pengols <pengelly30@hotmail.com>
2020-05-04 13:41:50 +0000

    hid gamebox on play.html

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -36,6 +36,17 @@ window.onclick = function(event) {
     }
 };
 
+// Game //
+
+function easyGame() {
+  var x = document.getElementById("gameBox");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
+
 // Audio //
 
 let bgm = document.getElementById("bgm");
