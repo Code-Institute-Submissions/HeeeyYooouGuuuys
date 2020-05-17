5d1f0e378b467a7c5d7ae2696f8f0034e7e785a2
pengols <pengelly30@hotmail.com>
2020-05-17 14:13:36 +0000

    commented out non-working code and built event listeners for overlay and card arrays

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -1,3 +1,106 @@
+// checks for load state of DOM //
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    let card = Array.from(document.getElementsByClassName("card"));
+    let overlay = Array.from(document.getElementsByClassName("overlay"));
+
+    // card.forEach(card => card.addEventListener("click", cardFlip));
+    // };
+    overlay.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            // game.startGame();
+        });
+    });
+    card.forEach(card => {
+        card.addEventListener('click', () => {
+            this.cardFlip(card);
+        });
+    });
+}
+
+
+
+
+/*function cardFlip() {
+    this.classList.toggle("flipped");
+}
+
+
+
+/*class cardSwap {
+    constructor(totalTime, cards) {
+        this.cardArray = cards;
+        this.totalTime = totalTime;
+        this.timer = document.getElementById("total-time");
+        this.counter = document.getElementById("total-moves");
+    }
+    startGame() {
+        this.totalMoves = 0;
+        this.matchedCards = [];
+        this.totalTime = 0;
+        this.audioEvents = new AudioEvents();
+    }
+}
+
+class AudioEvents {
+    constructor() {
+        this.gameOverAudio = new Audio("assets/audio/fail.wav");
+        this.gameSuccessAudio = new Audio("assets/audio/success.wav");
+        this.flipAudio = new Audio("assets/audio/card-flip.wav");
+        this.cardMatchAudio = new Audio("assets/audio/coin.wav");
+        this.cardNoMatchAudio = new Audio("assets/audio/incorrect.wav")
+        this.BGM = new Audio("assets/audio/Chiptronical.ogg")
+        BGM.volume  = 0.8;
+    }
+    gameOver() {
+        this.gameOverAudio.play()
+    }
+    gameSuccess() {
+        this.gameSuccessAudio.play()
+    }     
+    cardFlip() {
+        this.flipAudio.play();
+    }
+    cardMatch() {
+        this.cardMatchAudio.play()
+    }
+    cardNomatch() {
+        this.cardNoMatchAudio.play()
+    }
+}
+
+//BGM//
+
+/*let bgm = document.getElementById("bgm");
+let isPlaying = false;
+let musicButtonToggle = document.getElementById("music-button")
+let musicButtonToggleGame = document.getElementById("music-button-game")
+
+function togglePlay() {
+  if (isPlaying) {
+    bgm.pause()
+  } else {
+    bgm.play();
+  }
+};
+bgm.onplaying = function() {
+  isPlaying = true;
+  musicButtonToggle.innerHTML = "Music: on";
+  musicButtonToggleGame.innerHTML = "Music: on";
+};
+bgm.onpause = function() {
+  isPlaying = false;
+  musicButtonToggle.innerHTML = "Music: off";
+  musicButtonToggleGame.innerHTML = "Music: off"
+};
+
 // Modals
 
 let modal = document.getElementsByClassName('modal');
@@ -33,69 +136,22 @@ window.onclick = function(event) {
 // Game //
 
 // jQuery to hide difficulty-text and gameBox cards when difficulty button clicked//
-$('#gameBox').hide();
-$('.difficulty-button').on('click',
+/*$('#gameBox').hide();
+$('#start-button').on('click',
   function() {
     $('.difficulty-text, #gameBox').toggle();
   }
-);
-
-let cards = Array.from(document.getElementsByClassName("card"));
-
-class cardSwap {
-    constructor(totalTime, cards) {
-        this.cardArray = cards;
-        this.totalTime = totalTime;
-        this.timer = document.getElementById("total-time");
-        this.ticker = document.getElementById("total-moves");
-    }
-    startGame() {
-        this.totalMoves = 0;
-        this.matchedCards = [];
-        this.totalTime = 0;
-    }
-}
+);*/
 
 // flip card on click functionality
 
-let card = document.querySelectorAll(".card")
+// let card = document.querySelectorAll(".card")
 
-function cardFlip() {
+/* function cardFlip() {
     this.classList.toggle("flipped");
-};
+    
+} */
 
-card.forEach(card => card.addEventListener("click", cardFlip))
+// card.forEach(card => card.addEventListener("click", cardFlip))
 
-// Audio //
-
-let bgm = document.getElementById("bgm");
-let isPlaying = false;
-let musicButtonToggle = document.getElementById("music-button")
-let musicButtonToggleGame = document.getElementById("music-button-game")
-
-function togglePlay() {
-  if (isPlaying) {
-    bgm.pause()
-  } else {
-    bgm.play();
-  }
-};
-bgm.onplaying = function() {
-  isPlaying = true;
-  musicButtonToggle.innerHTML = "Music: on";
-  musicButtonToggleGame.innerHTML = "Music: on";
-};
-bgm.onpause = function() {
-  isPlaying = false;
-  musicButtonToggle.innerHTML = "Music: off";
-  musicButtonToggleGame.innerHTML = "Music: off"
-};
-
-
-
-
-
-
-
-
-
+// Audio /
