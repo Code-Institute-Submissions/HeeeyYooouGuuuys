960ebd86f610b9682ddebb2b4db9ef29ae0d6c09
pengols <pengelly30@hotmail.com>
2020-05-26 19:53:21 +0000

    added JS to show game over template overlay and all cards remove 'flipped' class on new game

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -1,7 +1,3 @@
-"use strict";
-
-let hasFlippedCard = false;
-let firstCard, secondCard;
 
 function ready() {
     let cards = Array.from(document.getElementsByClassName("card"));
@@ -34,10 +30,19 @@ class cardSwap {
         this.timer = 0;
         this.audioEvents = new AudioEvents();
         this.audioEvents.musicStart();
-        this.shuffleCards();
+        // this.shuffleCards();
         this.startTimer();
-        this.busy = true;
+        this.busy = false;
         this.cardToCheck = null;
+        console.log(this);
+        this.resetCards();
+
+    }
+
+    resetCards() {
+     for (var i = 0; i < this.cards.length; i++) {
+   this.cards[i].classList.remove('flipped');
+}
 
     }
 
@@ -48,14 +53,14 @@ class cardSwap {
         }, 1000);
     }
 
-        shuffleCards() {   
-        for(let i = this.cards.length - 1; i > 0; i--) {
-            let randomIndex = Math.floor(Math.random() * (i+1));
-            this.cards[randomIndex].style.order = i;
-            this.cards[i].style.order = randomIndex;
-        }
+    //     shuffleCards() {   
+    //     for(let i = this.cards.length - 1; i > 0; i--) {
+    //         let randomIndex = Math.floor(Math.random() * (i+1));
+    //         this.cards[randomIndex].style.order = i;
+    //         this.cards[i].style.order = randomIndex;
+    //     }
 
-    }
+    // }
 
         cardFlip(card) {
             if(this.canFlipCard(card)) {
@@ -63,33 +68,53 @@ class cardSwap {
             this.audioEvents.cardFlipAudio();
             this.counter += 1;
             this.totalMoves.innerHTML = "Moves: " + this.counter;
+            
+            if(this.cardToCheck)
+                this.checkForCardMatch(card);
+            else
+                this.cardToCheck = card;
             }
         }
         
-        canFlipCard(card) {
-            return true;
-            // return !this.busy && !this.matchedCards.includes(card) && card!==this.cardToCheck;
+    
+
+    checkForCardMatch(card) {
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else
+            this.cardMismatch(card, this.cardToCheck);
+
+            this.cardToCheck = null;
     }
-        // if(!card.hasFlippedCard) {
-        //     card.hasFlippedCard = true;
-        //     firstCard = event.currentTarget;
-        // } else {
-        //     card.hasFlippedCard = false;
-        //     secondCard = event.currentTarget;
-
-        //     console.log({firstCard, secondCard});
-        //     console.log(firstCard.dataset.framework);
-
-        //     if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        //         firstCard.removeEventListener("click");
-        //         secondCard.removeEventListener("click");
-        //     } else {
-        //         setTimeout(() => {
-        //         firstCard.classList.remove("flipped");
-        //         secondCard.classList.remove("flipped");
-        //     }, 1500);
-        // }
-        // }
+    getCardType(card) {
+        return card.getElementsByClassName("card-front")[0].src;
+    }
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        this.audioEvents.cardMatch();
+        if(this.matchedCards.length === this.cards.length)
+        this.gameOver();
+    }
+    cardMismatch(card1, card2) {
+        this.busy = true;
+        this.audioEvents.cardNomatch();
+        setTimeout(() => {
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        this.busy = false;
+        }, 1000);
+    }
+        canFlipCard(card) {
+            return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+        gameOver() {
+            this.audioEvents.musicFade();
+            this.audioEvents.gameSuccess();
+            document.getElementsByClassName("game-over-overlay")[0].classList.add("visible");
+            console.log(this);
+            console.log("gameover");
+        }
 
 }
 
@@ -220,3 +245,24 @@ $('#start-button').on('click',
 // card.forEach(card => card.addEventListener("click", cardFlip))
 
 // Audio /
+
+        // if(!card.hasFlippedCard) {
+        //     card.hasFlippedCard = true;
+        //     firstCard = event.currentTarget;
+        // } else {
+        //     card.hasFlippedCard = false;
+        //     secondCard = event.currentTarget;
+
+        //     console.log({firstCard, secondCard});
+        //     console.log(firstCard.dataset.framework);
+
+        //     if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        //         firstCard.removeEventListener("click");
+        //         secondCard.removeEventListener("click");
+        //     } else {
+        //         setTimeout(() => {
+        //         firstCard.classList.remove("flipped");
+        //         secondCard.classList.remove("flipped");
+        //     }, 1500);
+        // }
+        // }
