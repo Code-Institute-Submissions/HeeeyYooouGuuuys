3ab3b484e7e8765af263dfe4ccdcd937943d1ae0
pengols <pengelly30@hotmail.com>
2020-06-02 18:25:06 +0000

    removed commented out code that was no longer needed

--- assets/js/cardswap.js
+++ assets/js/cardswap.js
@@ -302,37 +302,7 @@ bgm.onpause = function() {
   musicButtonToggleGame.innerHTML = "Music: off"
 };
 
-// Modals
 
-let modal = document.getElementsByClassName('modal');
-let btn = document.getElementsByClassName("modalOpen");
-let span = document.getElementsByClassName("close");
-
-// Open modal 
-btn[0].onclick = function() {
-    modal[0].style.display = "block";
-}
-
-btn[1].onclick = function() {
-    modal[1].style.display = "block";
-}
-// close on x
-span[0].onclick = function() {
-    modal[0].style.display = "none";
-}
-
-span[1].onclick = function() {
-    modal[1].style.display = "none";
-}
-// click outside modal close
-window.onclick = function(event) {
-    if (event.target == modal[0]) {
-        modal[0].style.display = "none";
-    }
-    if (event.target == modal[1]) {
-        modal[1].style.display = "none";
-    }
-};
 
 // Game //
 
